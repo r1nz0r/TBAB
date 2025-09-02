8a6ed50c614939fd40c4611259beee81dc441b80
r1nz0r <nicecontra1@gmail.com>
2025-09-02 15:42:49 +0300

    Added weapon ids

--- TBABGame/src/core/Types.h
+++ TBABGame/src/core/Types.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string_view>
 
 namespace TBAB
 {
@@ -15,4 +16,14 @@ namespace TBAB
         Piercing,
         Bludgeoning
     };
+
+    namespace WeaponId
+    {
+        inline constexpr std::string_view WEAPON_CLUB{"WEAPON_CLUB"};
+        inline constexpr std::string_view WEAPON_DAGGER{"WEAPON_DAGGER"};
+        inline constexpr std::string_view WEAPON_SWORD{"WEAPON_SWORD"};
+        inline constexpr std::string_view WEAPON_AXE{"WEAPON_AXE"};
+        inline constexpr std::string_view WEAPON_SPEAR{"WEAPON_SPEAR"};
+        inline constexpr std::string_view WEAPON_LEGENDARY_SWORD{"WEAPON_LEGENDARY_SWORD"};
+    }
 } // namespace TBAB
