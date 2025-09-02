2a8a10e08ca9e6cfd2584b7697c96c41d1c47c71
r1nz0r <nicecontra1@gmail.com>
2025-09-02 14:18:40 +0300

    Created Types.h for types definition of game entities

---
+++ TBABGame/src/core/Types.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace TBAB
+{
+    struct Attributes
+    {
+        int strength;
+        int dexterity;
+        int endurance;
+    };
+
+    enum class DamageType
+    {
+        Slashing,
+        Piercing,
+        Bludgeoning
+    };
+} // namespace TBAB
