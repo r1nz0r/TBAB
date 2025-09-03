20e219799525cbafddf240ed10da5fcc3d7d762c
r1nz0r <nicecontra1@gmail.com>
2025-09-03 12:37:16 +0300

    Created basic implementation of Battle class

---
+++ TBABGame/src/core/systems/Battle.cpp
@@ -0,0 +1,59 @@
+﻿#include "Battle.h"
+#include "entities/Creature.h"
+#include "interfaces/IRenderer.h"
+
+#include <thread>
+#include <chrono>
+
+namespace TBAB
+{
+    static constexpr int PAUSE_TIME_IN_MS = 1000;
+    
+    Battle::Battle(Creature& combatant1, Creature& combatant2, IRenderer& renderer) 
+        : m_combatant1(combatant1), 
+          m_combatant2(combatant2),
+          m_renderer(renderer)
+    {
+        if (combatant1.GetAttributes().dexterity >= combatant2.GetAttributes().dexterity)
+        {
+            m_attacker = &combatant1;
+            m_defender = &combatant2;
+        }
+        else
+        {
+            m_attacker = &combatant2;
+            m_defender = &combatant1;
+        }
+    }
+    
+    BattleResult Battle::Start()
+    {
+        m_renderer.OnBattleStart(m_combatant1, m_combatant2);
+
+        while (m_combatant1.IsAlive() && m_combatant2.IsAlive())
+        {
+            m_renderer.OnTurnStart(*m_attacker);
+
+            const int damage = m_attacker->CalculateDamage();
+            m_defender->TakeDamage(damage);
+            
+            m_renderer.OnAttack(*m_attacker, *m_defender, damage);
+
+            std::swap(m_attacker, m_defender);
+            std::this_thread::sleep_for(std::chrono::milliseconds(PAUSE_TIME_IN_MS));
+        }
+
+        if (m_combatant1.IsAlive())
+        {
+            m_renderer.OnBattleEnd(m_combatant1);
+            return BattleResult::Combatant1_Won;
+        }
+        else
+        {
+            m_renderer.OnBattleEnd(m_combatant2);
+            return BattleResult::Combatant2_Won;
+        }
+        // Note: We don't handle Draw yet, but the system is ready for it.
+    }
+
+} // namespace TBAB
---
+++ TBABGame/src/core/systems/Battle.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+namespace TBAB
+{
+    class Creature;
+    class IRenderer; // Forward declaration
+
+    enum class BattleResult
+    {
+        Combatant1_Won,
+        Combatant2_Won,
+        Draw
+    };
+}
+
+namespace TBAB
+{
+    class Battle
+    {
+    public:
+        // Now constructor takes a renderer to decouple logic from presentation
+        Battle(Creature& combatant1, Creature& combatant2, IRenderer& renderer);
+        
+        // Returns a clear result
+        BattleResult Start();
+
+    private:
+        Creature& m_combatant1;
+        Creature& m_combatant2;
+        IRenderer& m_renderer; // Reference to the renderer
+        Creature* m_attacker;
+        Creature* m_defender;
+    };
+}
