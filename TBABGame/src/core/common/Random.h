7c2537b89d394b17c3640ffe043e63ebca7a2409
r1nz0r <nicecontra1@gmail.com>
2025-09-03 14:24:25 +0300

    Added simple RNG

---
+++ TBABGame/src/core/common/Random.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <random>
+
+namespace TBAB
+{
+    class Random
+    {
+    public:
+        Random() = delete;
+        Random(const Random&) = delete;
+        Random& operator=(const Random&) = delete;
+        Random(Random&&) = delete;
+        Random& operator=(Random&&) = delete;
+
+        // Return random generated number in range of [min, max]
+        static int Get(int min, int max)
+        {
+            static std::mt19937 generator(std::random_device{}());
+            std::uniform_int_distribution<int> distribution(min, max);
+
+            return distribution(generator);
+        }
+    };
+} // namespace TBAB
