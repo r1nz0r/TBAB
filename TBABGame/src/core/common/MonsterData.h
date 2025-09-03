b4593dc358754e758142a45b8acc23c387ea8f43
r1nz0r <nicecontra1@gmail.com>
2025-09-03 16:56:46 +0300

    Added monster data

---
+++ TBABGame/src/core/common/MonsterData.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "core/common/Types.h"
+#include <string>
+#include <vector>
+
+namespace TBAB
+{
+    struct MonsterData
+    {
+        std::string id;
+        std::string name;
+        int health;
+        Attributes attributes;
+        int innateDamage;
+        DamageType innateDamageType;
+        std::string droppedWeaponId;
+        std::vector<std::string> abilityIds;
+    };
+}
---
+++ TBABGame/src/core/common/WeaponData.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "core/common/Types.h"
+#include <string>
+
+namespace TBAB
+{
+    struct WeaponData
+    {
+        std::string id;
+        std::string name;
+        int damage;
+        DamageType damageType;
+    };
+}
