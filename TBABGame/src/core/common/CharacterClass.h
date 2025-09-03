35dc5ac63cdbe3f93c24dcabd2e6d762df6b109b
r1nz0r <nicecontra1@gmail.com>
2025-09-03 16:56:54 +0300

    added character class

---
+++ TBABGame/src/core/common/CharacterClass.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include "core/common/Types.h"
+#include "nlohmann/json.hpp"
+
+namespace TBAB
+{
+    struct LevelBonus
+    {
+        int level;
+        nlohmann::json bonusData;
+    };
+
+    class CharacterClass
+    {
+    public:
+        std::string id;
+        std::string name;
+        int healthPerLevel;
+        std::vector<LevelBonus> levelBonuses;
+    };
+}
