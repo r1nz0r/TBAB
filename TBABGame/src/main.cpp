9e86103ce69485227288c429d3068350cfbca036
r1nz0r <nicecontra1@gmail.com>
2025-09-04 13:21:54 +0300

    Updated main.cpp for tests

--- TBABGame/src/main.cpp
+++ TBABGame/src/main.cpp
@@ -26,31 +26,16 @@ int main()
 
     std::cout << "\nGame loaded successfully. Ready to start.\n";
 
-    // auto sword = dataManager.CreateWeapon("WEAPON_DAGGER");
-    //
-    // if (sword)
-    // {
-    //     std::cout << "Successfully created a test weapon: " << sword->GetName() << "\n";
-    // }
-    //
-    // auto skeleton = dataManager.CreateMonster(TBAB::MonsterId::MONSTER_GOLEM);
-    //
-    // if (skeleton)
-    // {
-    //     std::cout << "Created: " << skeleton->GetName() << "\n";
-    //     std::cout << "Health: " << skeleton->GetCurrentHealth() << "/" << skeleton->GetMaxHealth() << "\n";
-    //     std::cout << "Drops weapon: " << skeleton->GetDroppedWeaponName() << "\n";
-    //     std::cout << "Calculated Damage: " << skeleton->CalculateDamage() << "\n";
-    // }
+    TBAB::ConsoleRenderer renderer;
+    renderer.RegisterEventHandlers();
 
     TBAB::Player player("Hero", 15, {2, 2, 2}, dataManager.CreateWeapon(TBAB::WeaponId::WEAPON_DAGGER));
     auto monster = dataManager.CreateMonster(TBAB::MonsterId::MONSTER_DRAGON);
-    auto renderer = TBAB::ConsoleRenderer();
     
     if (monster)
     {
-        TBAB::Battle battle(player, *monster, renderer);
-        TBAB::BattleResult result = battle.Start();
+        TBAB::Battle battle(player, *monster);
+        battle.Start();
     }
 
     return 0;
