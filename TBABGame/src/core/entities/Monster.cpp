939b73cb4ebb564fb498fbcd661a0592e383dd5e
r1nz0r <nicecontra1@gmail.com>
2025-09-04 12:41:57 +0300

    Changed to weapon in creature constructor

--- TBABGame/src/core/entities/Monster.cpp
+++ TBABGame/src/core/entities/Monster.cpp
@@ -1,5 +1,5 @@
 #include "core/entities/Monster.h"
-#include "core/weapons/InnateAttack.h"
+#include "core/weapons/Weapon.h"
 #include <utility>
 
 namespace TBAB
@@ -7,7 +7,7 @@ namespace TBAB
 
     Monster::Monster(std::string name, Attributes attributes, const int health, const int innateDamage, DamageType innateDamageType,
         std::unique_ptr<Weapon> droppedWeapon)
-        : Creature(std::move(name), health, attributes, std::make_unique<InnateAttack>(innateDamage, innateDamageType)),
+        : Creature(std::move(name), health, attributes, std::make_unique<Weapon>("Innate Attack", innateDamage, innateDamageType)),
           m_droppedWeapon(std::move(droppedWeapon))
     {
     }
